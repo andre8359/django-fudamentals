146590c74b091453cf55c170beed832936f60db0
André Mateus Rodrigues Dantas <andre.dantas@autotrac.com.br>
2018-03-06 14:09:37 -0300

    Add first css/js files

--- player/views.py
+++ player/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from gameplay.models import Game
 
+
 def home(request):
     all_my_games = Game.objects.games_for_user(request.user)
     active_games = all_my_games.active()
